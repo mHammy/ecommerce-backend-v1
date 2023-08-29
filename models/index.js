5babf0171655367443db3840fa6f4b7e00629ccf
mHammy <133612424+mHammy@users.noreply.github.com>
2023-08-29 02:04:56 -0400

    Updated the models files to add functionality

--- models/index.js
+++ models/index.js
@@ -4,13 +4,21 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Set up associations
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+Category.hasMany(Product, { foreignKey: 'category_id' });
 
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
